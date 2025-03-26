49ae813f3c0284a0b41a8a74b33ef5cc950dc11a
DaniloSantos284 <daniloribeiro761@gmail.com>
2025-03-26 19:24:04 -0300

    Estrutura inicial do projeto

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState, useRef } from 'react'
+import axios from 'axios'
+import './App.css'
+import WeatherInformations from './components/WeatherInformations/WeatherInformations'
+
+function App() {
+  const [weather, setweather] = useState({})
+  const inputRef = useRef()
+
+async  function searchCity() {
+    const city = inputRef.current.value
+    const key = "07007ad438e673cc6df91c24945bea28"
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br`
+
+    const apiInfo = await axios.get(url)
+    setweather(apiInfo.data)
+
+    console.log(data)
+  }
+
+  return (
+   <div>
+    <h1>Danilo Santos Previsão do tempo</h1>
+    <input type="text" placeholder='Digite o nome da cidade'/>
+    <button onClick={searchCity}>Buscar</button>
+
+    <WeatherInformations />
+   </div>
+  )
+}
+
+export default App
